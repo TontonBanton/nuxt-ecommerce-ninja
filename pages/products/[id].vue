1aa8b13c4d09e4e207ff28f181c6407523794f94
TontonBanton <pancla13ulas@gmail.com>
2024-10-31 12:16:01 +0800

    Error page created, for all and id error

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,7 +9,9 @@
 
   const { id } = useRoute().params
   const uri = 'https://fakestoreapi.com/products/' + id
-
   const { data: product } = await useFetch(uri, { key: id })
+    if (!product.value) {
+      throw createError({ statusCode: 404, statusMessage: 'Product not found' })
+    }
 
 </script>
