5e7d20acf4c9a2796ea732f2456f7600568e562d
TontonBanton <pancla13ulas@gmail.com>
2024-10-31 13:32:48 +0800

    Initial version for sandy w/ logo

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="flex justify-center">
+      <img src="/joji2.jpg" alt="Joji Image" class="my-7 w-auto max-w-sm" />
+    </div>
     <h2>About</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus magnam ipsum nostrum, porro atque eius
       expedita itaque distinctio reiciendis nesciunt at officiis quis architecto vel laudantium dolorem! Aut, provident sapiente.</p>
