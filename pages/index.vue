78ddcdf46acd45fb4c657de6ac358eaee1586a3a
TontonBanton <pancla13ulas@gmail.com>
2024-10-31 13:08:12 +0800

    School logo added in the middle of home

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="flex justify-center">
+      <img src="/joji.png" alt="Joji Image" class="my-7 w-full max-w-sm" />
+    </div>
     <h2>Home</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad est quos obcaecati, placeat ducimus,
       corrupti sint pariatur nam, tempore aliquam veniam. Neque harum eum quasi minima consequuntur dolor nemo in!</p>
